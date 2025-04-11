763f19adeda8cc3327a122863549b7113e7beb4e
Samuel St Jean <92886388+samuel-stjean@users.noreply.github.com>
2025-04-10 22:45:05 -0400

    Update db_config.php

--- backend/api/db_config.php
+++ backend/api/db_config.php
@@ -2,7 +2,7 @@
 <?php
 $servername = "mysql.cise.ufl.edu";
 $username = "samuel.stjean";
-$password = "U2F4mg3vNKBfPBr";
+$password = "pw";
 $dbname = "Critter_Keeper_DB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
